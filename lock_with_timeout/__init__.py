d2162b32f0d6b6d78163c208e2e909be67905f77
Yoshiaki Ono <ono.kirin@gmail.com>
2018-07-25 14:42:35 +0900

    first commit

---
+++ lock_with_timeout/__init__.py
@@ -0,0 +1,53 @@
+__version__ = '0.1.0'
+__author__ = 'fx-kirin <fx.kirin@gmail.com>'
+__all__ = ['Lock']
+
+"""
+Thread-safe lock mechanism with timeout support module.
+"""
+
+from threading import ThreadError, current_thread
+from Queue import Queue, Full, Empty
+
+class Lock(object):
+    """
+    Thread-safe lock mechanism with timeout support.
+    """
+
+    def __init__(self):
+        self._queue = Queue(maxsize=1)
+        self._owner = None
+
+    def acquire(self, timeout=0):
+        th = current_thread()
+        try:
+            self._queue.put(
+                th, block=(timeout != 0),
+                timeout=(None if timeout < 0 else timeout)
+            )
+        except Full:
+            raise ThreadError('Lock Timed Out')
+
+        self._owner = th
+        return True
+
+    def release(self):
+        th = current_thread()
+        if th != self._owner:
+            raise ThreadError('This lock isn\'t owned by this thread.')
+
+        self._owner = None
+        try:
+            self._queue.get(False)
+            return True
+        except Empty:
+            raise ThreadError('This lock was released already.')
+    
+    def __enter__(self,*args, **kwargs):
+        if 'timeout' in kwargs:
+            self.acquire(timeout=kwargs['timeout'])
+        else:
+            self.acquire()
+    
+    def __exit__(self, *args, **kwargs):
+        self.release()
